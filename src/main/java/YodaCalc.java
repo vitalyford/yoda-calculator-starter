4155d7d600ad98e258cd68119d01666391cdf081
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2022-10-13 17:54:57 +0000

    Change linkedlist to yoda calc and clean things up

---
+++ src/main/java/YodaCalc.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class YodaCalc {
+    /**
+     * This method takes in the input string, 
+     * evaluates the math expression, and
+     * returns the result as a decimal value.
+     * 
+     * @param   input   the input string, supporting: +, -, *, /, (, )
+     * @return          the evaluated result
+     */
+    public static double calc(String input) {
+        return 0.0;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("591 + 10 * 6 / (442 - 3 * ( 61 + 7 - 8 )) + 4 / 2");
+    }
+}
---
+++ src/test/java/YodaCalcTests.java
@@ -0,0 +1,15 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+/** 
+ * JUnit tests cases for the Yoda Calculator.
+ * 
+ * @author Vitaly Ford
+ */ 
+public class LinkedListTests {
+    
+}
